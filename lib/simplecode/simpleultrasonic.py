3528ca3c718a2a9cf3630aac92227ab908beed1a
gcampbe95 <gcampbe95@charlottesvilleschools.org>
2023-09-12 11:40:14 -0400

    simplecode version of ultrasonic

---
+++ lib/simplecode/simpleultrasonic.py
@@ -0,0 +1,13 @@
+# test
+
+import board
+import time
+import adafruit_hcsr04
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+
+while True:
+    try:
+        print((sonar.distance,))
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.1)
