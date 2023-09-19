9b9bdd28c8f8474d9918665d808747e16bee5ce9
gcampbe95 <gcampbe95@charlottesvilleschools.org>
2023-09-19 11:11:36 -0400

    adding fade to ultrasonic_rainbow

--- ultrasonic_rainbow.py
+++ ultrasonic_rainbow.py
@@ -6,9 +6,19 @@ import adafruit_hcsr04
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 from rainbowio import colorwheel
 import neopixel 
+r = 255
+g = 0
+b = 0
+
+color = (r, g, b)
+red = (255, 0, 0) # 5
+magenta = (255, 0, 255) # 12.5
+blue = (0, 0, 255) # 20
+cyan = (0, 255, 255) # 27.5
+green = (0, 255, 0) # 35
 
 num_pixels = 1  # Update this to match the number of LEDs.
-BRIGHTNESS = 0.2  # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
+BRIGHTNESS = 0.4  # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, num_pixels, brightness=BRIGHTNESS, auto_write=False)
 
@@ -16,13 +26,26 @@ while True:
     try:
         print((sonar.distance,))
         if sonar.distance <= 5:
-            pixels.fill((255, 0, 0))
+            pixels.fill(red)
             pixels.show()
         if sonar.distance >= 35:
-            pixels.fill((0, 255, 0))
+            pixels.fill(green)
             pixels.show()
-        if sonar.distance == 20:
-            pixels.fill((0, 0, 255))
+        if (5 < sonar.distance) and (sonar.distance <= 12.5):
+            b = (b + 1) % 256
+            pixels.fill(colorwheel(color))
+            pixels.show()
+        if (12.5 < sonar.distance) and (sonar.distance <= 20):
+            r = (r - 1) % 256
+            pixels.fill(colorwheel(color))
+            pixels.show()
+        if (20 < sonar.distance) and (sonar.distance <= 27.5):
+            g = (g + 1) % 256
+            pixels.fill(colorwheel(color))
+            pixels.show()
+        if (27.5 < sonar.distance) and (sonar.distance < 35):
+            b = (b - 1) % 256
+            pixels.fill(colorwheel(color))
             pixels.show()
     except RuntimeError:
         print("Retrying!")
