7d0d1be8834d64bbddd40b7ff2b5706b856d96f0
gcampbe95 <gcampbe95@charlottesvilleschools.org>
2023-09-22 11:20:37 -0400

    ultrasonic_rainbow is finally functional

--- ultrasonic_rainbow.py
+++ ultrasonic_rainbow.py
@@ -7,11 +7,11 @@ sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 from rainbowio import colorwheel
 import neopixel
 from adafruit_simplemath import map_range
-from adafruit_simplemath import map_unconstrained_range
+from adafruit_simplemath import map_unconstrained_range # I opted for unconstrained for this because I'm sometimes mapping color values backwards
 
-r = 255
+r = 0 # these will change with distance
 g = 0
-b = 0
+b = 0 
 
 red = (255, 0, 0) # 5
 magenta = (255, 0, 255) # 12.5
@@ -19,40 +19,56 @@ blue = (0, 0, 255) # 20
 cyan = (0, 255, 255) # 27.5
 green = (0, 255, 0) # 35
 
-num_pixels = 1  # Update this to match the number of LEDs.
-BRIGHTNESS = 0.4  # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
+num_pixels = 1  # only 1 because I'm using my neopixel
+BRIGHTNESS = 0.4  # between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, num_pixels, brightness=BRIGHTNESS, auto_write=False)
 
 while True:
     try:
         print((sonar.distance,))
-        if sonar.distance <= 5:
-            pixels.fill(red)
-            pixels.show()
-        if sonar.distance >= 35:
+        if sonar.distance <= 5: #--------------------------------------------------------------------------------------------------------
+            pixels.fill(red) # set color to red for values less than or equal to 5 
+            pixels.show() # display color
+        if sonar.distance >= 35: #-------------------------------------------------------------------------------------------------------
             pixels.fill(green)
             pixels.show()
-        if (5 < sonar.distance) and (sonar.distance <= 12.5):
-            map_unconstrained_range(b, 0, 255, sonar.distance, 5, 12.5)
+        if (5 < sonar.distance) and (sonar.distance <= 12.5): #--------------------------------------------------------------------------
+            # color = (red)
+            r = 255 #setting initial rgb values
+            g = 0
+            # b = 0
+            b = map_unconstrained_range(sonar.distance, 5, 12.5, 0, 255) # b icreases with distance
             color = (r, g, b)
             pixels.fill((color))
             pixels.show()
-        if (12.5 < sonar.distance) and (sonar.distance <= 20):
-            map_unconstrained_range(r, 255, 0, sonar.distance, 12.5, 20)
+        if (12.5 < sonar.distance) and (sonar.distance <= 20): #-------------------------------------------------------------------------
+            # color = (magenta)
+            # r = 255
+            g = 0
+            b = 255
+            r = map_unconstrained_range(sonar.distance, 12.5, 20, 255, 0)
             color = (r, g, b)
             pixels.fill((color))
             pixels.show()
-        if (20 < sonar.distance) and (sonar.distance <= 27.5):
-            map_unconstrained_range(g, 0, 255, sonar.distance, 20, 27.5)
+        if (20 < sonar.distance) and (sonar.distance <= 27.5): #-------------------------------------------------------------------------
+            # color = (blue)
+            r = 0
+            # g = 0
+            b = 255
+            g = map_unconstrained_range(sonar.distance, 20, 27.5, 0, 255)
             color = (r, g, b)
             pixels.fill((color))
             pixels.show()
-        if (27.5 < sonar.distance) and (sonar.distance < 35):
-            map_unconstrained_range(b, 255, 0, sonar.distance, 27.7, 35)
+        if (27.5 < sonar.distance) and (sonar.distance < 35): #--------------------------------------------------------------------------
+            # color = (cyan)
+            r = 0
+            g = 255
+            # b = 255
+            b = map_unconstrained_range(sonar.distance, 27.5, 35, 255, 0)
             color = (r, g, b)
             pixels.fill((color))
             pixels.show()
-    except RuntimeError:
+    except RuntimeError: #---------------------------------------------------------------------------------------------------------------
         print("Retrying!")
     time.sleep(0.1)
