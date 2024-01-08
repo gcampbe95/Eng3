3a6f63259a2e07427f01e14573920e2096737caf
gcampbe95 <gcampbe95@charlottesvilleschools.org>
2024-01-08 11:38:32 -0500

    fixing photoint code

--- photointerrupter.py
+++ photointerrupter.py
@@ -5,6 +5,7 @@ from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 counter = 0
 lcd = LCD(I2CPCF8574Interface(board.I2C(), 0x27), num_rows=2, num_cols=16)
+now = time.monotonic()
 
 photoint = digitalio.DigitalInOut(board.D6)
 photoint.direction = digitalio.Direction.INPUT
@@ -17,16 +18,15 @@ lcd.print("interrupts:")
 while True:
     lcd.set_cursor_pos(0,0)
     lcd.print("interrupts:  ")
-    if not photoint.value and photoint_state is None:
-        photoint_state = None 
-        lcd.set_cursor_pos(1,0)
-        lcd.print("                 ")
+    if not photoint.value and photoint_state == "interrupted":
+        photoint_state = None
     if photoint.value and photoint_state is None:
         photoint_state = "interrupted"  
-        photoint_state = None
-        counter = counter + 1
+        counter += 1
         print(counter)
+    if (now + 4) < time.monotonic():
         lcd.set_cursor_pos(1,0)
-        lcd.print(counter)
+        lcd.print(str(counter))
+       
     
 
